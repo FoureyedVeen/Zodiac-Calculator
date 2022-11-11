ab4d9c4b08f6faa487333ad80fe4a6a7af010e95
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-11-11 19:39:13 +0100

    can't wrap my head around the logic

---
+++ calc.js
@@ -0,0 +1,10 @@
+const display = document.getElementById("display");
+
+const day = document.getElementById("day");
+const months = document.getElementById("months")
+// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// console.log( months);
+
+months.addEventListener("click", function months() {
+    console.log(months);
+});
