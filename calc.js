fa09007959b75dbd2245eae7c223f4e4b07d055c
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-11-13 13:48:26 +0100

    changed select element to input

--- calc.js
+++ calc.js
@@ -1,10 +1,5 @@
 const display = document.getElementById("display");
 
-const day = document.getElementById("day");
-const months = document.getElementById("months")
-// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-// console.log( months);
-
-months.addEventListener("click", function months() {
-    console.log(months);
-});
+let day = document.getElementById("birth-day");
+console.log(day);
+const months = document.getElementById("birth-month")
